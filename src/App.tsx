b5d1cc4886a9f514b89d7d77831141db05f48211
goro-ne <hayao56@gmail.com>
2024-08-21 12:27:10 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import { Todo } from './Todo';
+
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
+  const onClickFetchData = () => {
+    axios
+      .get<Array<TodoType>>('https://jsonplaceholder.typicode.com/todos')
+      .then((res) => {
+        res.data.map;
+        setTodos(res.data);
+        console.log(res.data);
+      });
+  };
+  return (
+    <>
+      <div>
+        <button onClick={onClickFetchData}>データ取得</button>
+        {todos.map((todo) => {
+          return (
+            <Todo
+              key={todo.id}
+              title={todo.title}
+              userId={todo.userId}
+              completed={todo.completed}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
